9db8659569572aa4742710449c39b944edcc6526
mitraillet <steve.henriquet@gmail.com>
2018-10-30 13:18:15 +0100

    setup project

---
+++ app/src/main/java/com/mitraillet/sym_labo2_data/MainActivity.java
@@ -0,0 +1,62 @@
+package com.mitraillet.sym_labo2_data;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mClickButton1 = findViewById(R.id.async);
+        Button mClickButton2 = findViewById(R.id.diff);
+        Button mClickButton3 = findViewById(R.id.object);
+        Button mClickButton4 = findViewById(R.id.JSON);
+        Button mClickButton5 = findViewById(R.id.XML);
+
+        mClickButton1.setOnClickListener(onClickListener);
+        mClickButton2.setOnClickListener(onClickListener);
+        mClickButton3.setOnClickListener(onClickListener);
+        mClickButton4.setOnClickListener(onClickListener);
+        mClickButton5.setOnClickListener(onClickListener);
+    }
+
+    // somewhere else in your code
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.async: {
+                    // do something for button 1 click
+                    Intent intent = new Intent(MainActivity.this, com.mitraillet.sym_labo2_data.async.class);
+                    startActivity(intent);
+                    break;
+                }
+
+                case R.id.diff: {
+                    // do something for button 2 click
+                    break;
+                }
+
+                case R.id.object: {
+                    // do something for button 1 click
+                    break;
+                }
+
+                case R.id.JSON: {
+                    // do something for button 2 click
+                    break;
+                }
+
+                case R.id.XML: {
+                    // do something for button 2 click
+                    break;
+                }
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/mitraillet/sym_labo2_data/async.java
@@ -0,0 +1,14 @@
+package com.mitraillet.sym_labo2_data;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class async extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.async);
+
+    }
+}
