5b0305241135b5450cc651ee2cddae0e87dde0c6
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2018-11-13 09:40:55 +0100

    commit berfore checkout

--- app/src/main/java/com/mitraillet/sym_labo2_data/XMLDataSend.java
+++ app/src/main/java/com/mitraillet/sym_labo2_data/XMLDataSend.java
@@ -8,7 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import org.xmlpull.v1.XmlSerializer;
+
+import javax.xml.parsers.DocumentBuilder;
+
 public class XMLDataSend extends AppCompatActivity  {
+
+    private final int PHONE_NUMBER_SIZE = 10;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
@@ -21,20 +27,32 @@ public class XMLDataSend extends AppCompatActivity  {
 
         EditText firstName = findViewById(R.id.firstName);
         EditText lastName  = findViewById(R.id.lastName);
-        EditText phoneNumber = findViewById(R.id.phoneNumber);
+        final EditText phoneNumber = findViewById(R.id.phoneNumber);
         Button   sendButton  = findViewById(R.id.sendButton);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                // Make sure every field is complete
+                // Make sure every field is complete, otherwise we complete the lastname field
+                if (firstName == null && lastName == null ) {
+                    firstName.setText("");
+                    lastName.setText("Unnamed");
+                }
 
+                if (isValidNumber(phoneNumber.getText().toString())) {
+                    // Throw an error or write something in red...
+                }
                 // Prepare the XML form to send to the server
 
                 // Send it !
             }
         });
+    }
+
+    public boolean isValidNumber(String phone) {
+        // Or find a super extra regex to match phone number validation
+        return phone.length() >= PHONE_NUMBER_SIZE && !phone.matches("A-Za-z");
     }
 
     @Override
@@ -46,10 +64,10 @@ public class XMLDataSend extends AppCompatActivity  {
 
     }
 
-    private void setXMLForm(){
+    private void setXMLForm() {
 
         // Get data from the fields
-
+        DocumentBuilder builder;
         // Transform it to XML
     }
 
